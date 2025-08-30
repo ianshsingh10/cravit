ed3be52f5020e9a77d845647b3f4ae93efa93be1
Ansh Singh <ianshsingh10@gmail.com>
2025-08-30 21:47:34 +0530

    log changes

--- src/app/api/seller/route.js
+++ src/app/api/seller/route.js
@@ -4,11 +4,8 @@ import User from "@/models/user";
 
 export async function GET() {
   try {
-    console.log("Attempting to connect to the database...");
     await dbConnect();
-    console.log("Database connection successful.");
-    const sellers = await User.find({ role: "seller" }, { name: 1, email: 1, _id: 1 });
-    console.log("Sellers found:", sellers);
+    const sellers = await User.find({ role: "seller" });
 
     return NextResponse.json(sellers, { status: 200 });
   } catch (error) {
